f341a5982390104dc30e01f3894fd4dc77995caf
m-tsai <m-tsai@netmile.co.jp>
2014-06-10 10:34:33 +0900

    nagios update

--- nagios/recipes/default.rb
+++ nagios/recipes/default.rb
@@ -8,6 +8,8 @@ plugin_dir = "/usr/lib/nagios/plugins"
 cookbook_file "#{plugin_dir}/check_logfiles" do
   source "check_logfiles"
   mode 0755
+  owner 'root'
+  group 'root'
 end
 
 ##################
@@ -20,18 +22,24 @@ plugin_conf_dir = "/etc/nagios-plugins/config"
 cookbook_file "#{conf_dir}/nrpe.cfg" do
   source "nrpe.cfg"
   mode 0644
+  owner 'root'
+  group 'root'
 end
 
 # nrpe_local.cfg
 cookbook_file "#{conf_dir}/nrpe_local.cfg" do
   source "nrpe_local.cfg"
   mode 0644
+  owner 'root'
+  group 'root'
 end
 
 # check_logfiles.cfg
 cookbook_file "#{plugin_conf_dir}/check_logfiles.cfg" do
   source "check_logfiles.cfg"
   mode 0644
+  owner 'root'
+  group 'root'
 end
 
 ##################
